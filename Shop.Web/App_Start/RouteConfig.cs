89bdde294a015435467708ca6f21afb14699f823
Shiva Razzagh Karimi <shiva.razzaghkarimi@gmail.com>
2014-10-19 21:35:27 +0100

    fixed routing issue

--- Shop.Web/App_Start/RouteConfig.cs
+++ Shop.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Shop.Web
@@ -13,11 +9,13 @@ namespace Shop.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("test",
+                "Product/{id}",
+                new { controller = "Product", action = "Index", id = UrlParameter.Optional });
+
+            routes.MapRoute("Default", "{controller}/{action}/{id}",
+                new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                );
         }
     }
 }
